20efbbcb939cd89f55dfbe6fd61832af34b04fc6
Vincent Samaco <vsamaco@gmail.com>
2012-07-26 16:12:45 -0700

    defined collections and views

--- scripts/vstwitter.js
+++ scripts/vstwitter.js
@@ -7,3 +7,51 @@ var Tweet = Backbone.Model.extend({
     }
   }
 });
+
+var TweetCollection = Backbone.Collection.extend({
+  model: Tweet
+});
+
+var TweetView = Backbone.View.extend({
+  tagName: 'li',
+  template: _.template($("#tweet-template")),
+  
+  initialize: function() {
+    this.model.on('change', this.render, this);
+    this.model.on('destroy', this.remove, this);
+  },
+  
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+  },
+  
+  remove: function() {
+    this.remove();
+  }
+});
+
+var TweetsView = Backbone.View.extend({
+  initialize: function() {
+    this.collection.on('add', this.addOne, this);
+    this.collection.on('reset', this.addAll, this);
+  },
+  
+  render: function() {
+    this.addAll();
+    return this;
+  },
+  
+  addOne: function(tweet) {
+    var view = new TweetView({model: tweet});
+    this.$el.append(view.render().el);
+  },
+  
+  addAll: function() {
+    this.$el.empty();
+    this.collection.forEach(this.addOne, this);
+  }
+});
+
+var TweetApp = Backbone.View.extend({
+  
+});
